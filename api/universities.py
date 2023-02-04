6827ba2d959a169b7e5ba3367b914510477c5073
JaiPrakash-Saarthi <jai.prakash@saarthi.ai>
2023-02-04 20:24:02 +0530

    finally learn fastapi+celery

---
+++ api/universities.py
@@ -0,0 +1,12 @@
+def get_all_universities_for_country(country: str) -> dict:
+    
+    url = 'http://universities.hipolabs.com/search'
+    params = {'country': country}
+    client = httpx.Client()
+    response = client.get(url, params=params)
+    response_json = json.loads(response.text)
+    universities = []
+    for university in response_json:
+        university_obj = University.parse_obj(university)
+        universities.append(university_obj)
+    return {country: universities}
